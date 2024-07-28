6aec08065071115cfd966bf95a1a0efa381e6135
unknown <carloscruzfotografo17mail.com>
2024-07-28 09:30:44 -0600

    segundo commit

--- app/src/main/java/com/example/segundoparcial/SplashActivity.java
+++ app/src/main/java/com/example/segundoparcial/SplashActivity.java
@@ -3,6 +3,9 @@ package com.example.segundoparcial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,9 +15,18 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Obtener el ImageView del logo
+        ImageView logo = findViewById(R.id.logo);
+
+        // Cargar la animación
+        Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_logo);
+
+        // Aplicar la animación al logo
+        logo.startAnimation(rotateAnimation);
+
         // Retraso de 3 segundos antes de iniciar la actividad principal
         new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, com.example.segundoparcial.MenuActivity.class));
+            startActivity(new Intent(SplashActivity.this, MenuActivity.class));
             finish();
         }, 3000);
     }
